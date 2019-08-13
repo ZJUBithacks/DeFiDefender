5a07f2fa334635cb71cb7fa4de80cd50dca4ec99
Bingyy <bing@bingw.me>
2019-08-13 21:40:01 +0800

    [BING] update class defination company, govenment, user

---
+++ defidefender_frontend/src/_pages/user/loan/loan.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+import { withStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container'
+import Table from '@material-ui/core/Table'
+import TableBody from '@material-ui/core/TableBody'
+import TableCell from '@material-ui/core/TableCell'
+import TableHead from '@material-ui/core/TableHead'
+import TableRow from '@material-ui/core/TableRow'
+import Paper from '@material-ui/core/Paper'
+import { Button } from '@material-ui/core'
+import CloudUploadIcon from '@material-ui/icons/CloudUpload'
+import { Link } from 'react-router-dom'
+
+const useStyles = theme => ({
+    userInfo: {
+        textAlign: 'center'
+    },
+    content: {
+        flexGrow: 1,
+        height: '100vh',
+        overflow: 'auto',
+    },
+    container: {
+        paddingTop: theme.spacing(4),
+        paddingBottom: theme.spacing(4),
+    },
+    paper: {
+        padding: theme.spacing(2),
+        display: 'flex',
+        overflow: 'auto',
+        flexDirection: 'column',
+    },
+    fixedHeight: {
+        height: 240,
+    },
+});
+
+class Blacklist extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+const LoanWithStyles = withStyles(useStyles)(Loan)
+
+export { LoanWithStyles as Loan };
