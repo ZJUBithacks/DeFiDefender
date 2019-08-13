673482593f7d4cebb478727ea1dde24f0c084d9c
tygavin <tianyao@zju.edu.cn>
2019-08-13 23:38:12 +0800

    [Gavin] simple Modify

--- defidefender_frontend/src/App.js
+++ defidefender_frontend/src/App.js
@@ -2,10 +2,6 @@ import React, { Component } from 'react'
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
 import { Layout } from './_pages/layout'
 import { HomeContent } from './_pages/homeContent'
-import { AllRequestsContent, YourRequestsContent, PushRequestContent, PushResponseContent } from './_pages/request'
-import { YourResponsesContent, ReceivedResponsesContent } from './_pages/response'
-import { RecordsContent } from './_pages/records'
-import { NotificationsContent } from './_pages/notifications'
 import { UserInfo } from './_pages/government'
 
 class App extends Component {
